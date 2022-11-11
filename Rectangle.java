27f25490d5117045e8d48b14d7ea1bb75a63a9d2
five-high-seas <117937319+five-high-seas@users.noreply.github.com>
2022-11-11 13:20:13 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -9,6 +9,11 @@ public class Rectangle
         height = rectHeight;
     }
     
+    public int getPerimeter()
+    {
+        return (width * 2) + (height * 2);
+    }
+    
     public int getArea()
     {
         return width * height;
@@ -23,7 +28,7 @@ public class Rectangle
     {
         return width;
     }
-        
+    
     public String toString()
     {
         return "Rectangle with width: " + width + " and height: " + height + ".\nTherefore, the box has dimensions of " + width + "x" + height;
